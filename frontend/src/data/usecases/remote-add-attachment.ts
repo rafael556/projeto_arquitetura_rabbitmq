1b30e5edeeafd079c604be544040d64d9972abcc
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-12-07 23:43:44 -0300

    feat(frontend): create data attachment remote usecases

---
+++ frontend/src/data/usecases/remote-add-attachment.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpStatusCode } from "@/data/protocols/http";
+import { AddAttachment } from "@/domain/usecases";
+import { UnexpectedError } from "@/domain/errors";
+
+export class RemoteAddAttachment implements AddAttachment {
+	constructor(private readonly url: string, private readonly httpClient: HttpClient<RemoteAddAttachment.Model>) {}
+
+	async add(params: AddAttachment.Params): Promise<AddAttachment.Model> {
+		const httpResponse = await this.httpClient.request({
+			url: this.url,
+			method: "post",
+			body: params,
+		});
+		switch (httpResponse.statusCode) {
+			case HttpStatusCode.ok:
+				return httpResponse.body!;
+			default:
+				throw new UnexpectedError();
+		}
+	}
+}
+
+export namespace RemoteAddAttachment {
+	export type Model = AddAttachment.Model;
+}
